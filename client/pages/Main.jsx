157fd22ff5af2b46d46b513ff74989110ecc555e
Sattwik <hellosattwik@gmail.com>
2022-06-12 05:03:55 +0530

    added drawer and fixed navbar

--- client/pages/Main.jsx
+++ client/pages/Main.jsx
@@ -1,12 +1,125 @@
-import React from 'react';
+// import React from 'react';
 import Navbar from '../components/Navbar';
 import Anchor from '../components/Anchor';
-export default function Main() {
+import Home from '../components/Home';
+// export default function Main() {
+//   return (
+//     <>
+
+//       <Anchor />
+//       <div>Main</div>
+//     </>
+//   );
+// }
+
+import React, { useState } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+const drawerWidth = 240;
+import { useLocation } from 'react-router-dom';
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'space-between',
+}));
+
+export default function MainPage() {
+  //   const [user, setUser] = useState(useLocation().state.user);
+
+  // console.log(user);
+
+  //   const [value, setValue] = React.useState(currentDoc ? currentDoc.body : '');
+
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <>
-      <Navbar />
-      <Anchor />
-      <div>Main</div>
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Navbar
+        open={open}
+        handleDrawerOpen={handleDrawerOpen}
+        drawerWidth={drawerWidth}
+        theme={theme}
+      />
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          justifyContent: 'space-between',
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            backgroundColor: '#c7f9cc',
+          },
+        }}
+        variant='persistent'
+        anchor='left'
+        open={open}
+      >
+        <DrawerHeader>
+          <Stack direction='row' sx={{ alignItems: 'center' }} spacing={1}>
+            <Avatar sx={{ backgroundColor: '#212529' }}>S</Avatar>
+            <Typography variant='h5' component='p'>
+              Satty
+            </Typography>
+          </Stack>
+
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <Anchor />
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        {/* <Home /> */}
+      </Main>
+    </Box>
   );
 }
