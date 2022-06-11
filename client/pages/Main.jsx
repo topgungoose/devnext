8bfd4d2969b80194c9d99e75fa1b94998eda1cf1
Sattwik <hellosattwik@gmail.com>
2022-06-12 02:46:26 +0530

    added anchor

--- client/pages/Main.jsx
+++ client/pages/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-
+import Anchor from '../components/Anchor';
 export default function Main() {
   return (
     <>
       <Navbar />
+      <Anchor />
       <div>Main</div>
     </>
   );
