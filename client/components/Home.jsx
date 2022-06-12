6198f6d7c9b0bc98fb1d799a473094aede3d08a7
lawyeh391 <lawyeh391@gmail.com>
2022-06-12 00:08:33 -0500

    added post and dialog

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductContainer from './ProductContainer';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import '../styles/Home.css';
+import Dialog from '@mui/material/Dialog';
+import Post from './Post';
 
 export default function Home() {
+  const [ open, setOpen ] = useState(false);
+  const handleClose = () => setOpen(false)
   return (
     <div className='home'>
       <div className='container'>
@@ -14,9 +18,17 @@ export default function Home() {
         color='primary'
         sx={{ position: 'absolute', top: 0, right: 0 }}
         aria-label='add'
+        onClick = {() => setOpen(true)}
       >
         <AddIcon />
       </Fab>
+      <Dialog 
+      fullWidth 
+      open={open}
+      maxWidth="sm"
+      >
+        <Post handleClose={handleClose}/>
+      </Dialog>
     </div>
   );
 }
