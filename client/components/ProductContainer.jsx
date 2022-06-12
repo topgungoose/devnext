9012dc9b8b6c7ddbd4ca810e48da0a141a5d99fa
Jin Yoo <iyoojin@gmail.com>
2022-06-12 17:26:57 -0400

    worked on product modal

--- client/components/ProductContainer.jsx
+++ client/components/ProductContainer.jsx
@@ -2,16 +2,17 @@ import * as React from 'react';
 import ProductItem from './ProductItem';
 import Grid from '@mui/material/Grid';
 
-export default function ProductContainer() {
+export default function ProductContainer({handleOpen}) {
   return (
     <Grid
       container
       spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
+      columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}
     >
       {Array.from(Array(20)).map((_, index) => (
-        <Grid item xs={4} sm={4} md={4} key={index}>
-          <ProductItem />
+        <Grid item xs={4} sm={4} md={4} lg={4} key={index}>
+          <ProductItem
+          handleOpen={handleOpen} />
         </Grid>
       ))}
     </Grid>
