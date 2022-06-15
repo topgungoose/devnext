b85d07e1126d5c9669ff43badd39d4ff2fb65c93
MockTech <83368864+MockTech@users.noreply.github.com>
2022-06-15 15:11:46 -0400

    Refactored Login.jsx - Added JSDoc for functions - Condenced import statements

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,9 +1,7 @@
 // TODO: STRETCH Feature: Modularize the code to work for Sign up and Login Component
 
-// TODO: Group imports based on source
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Avatar, Typography } from '@mui/material';
@@ -14,61 +12,72 @@ import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Navigates to particular route
 import cover from '../assets/cover.png';
 
+/**
+ * Login - Returns Login components
+ * @returns {component} Login Component
+ */
 export default function Login() {
   const [state, setState] = useState({ username: '', password: '' });
-  // navigate is used whenever we want to navigate to another route, provided by react-router-dom
+
+  /** 
+   * navigate - reroutes user to Route passing in State.
+   * @param {string} Route
+   * @param {object} State
+   */
   const navigate = useNavigate();
-  //******************* MUI **************************
-  //Paperstyle injected into components "style" in return
-  // MUI TODO: Make an single Object called stylesMUI and have these objects as the properties {}
-  const paperStyle = {
-    padding: 20,
-    height: '60vh',
-    width: 600,
-    margin: '20px auto',
-  };
 
-  const avatarStyle = { backgroundColor: '#FF6F61' };
-  const btnStyle = { margin: '8px 0' };
-  // ***********************************************
+    //******************* MUI **************************
+    const paperStyle = {
+      padding: 20,
+      height: '60vh',
+      width: 600,
+      margin: '20px auto',
+    };
+    const avatarStyle = { backgroundColor: '#FF6F61' };
+    const btnStyle = { margin: '8px 0' };
+    // ***********************************************
 
-  // submitHandler -> invoked when 'Sign In' button is clicked, When login form is submitted, perform fetch request, update state.
+  /**
+   * submitHandler - When login form is submitted, perform a fetch request, and update state.
+   * @param {object} event 
+   */
   const submitHandler = (event) => {
-    event.preventDefault(); // prevents page from refreshing
+    event.preventDefault(); // Prevents page from refreshing
     console.log(state);
     if (!state.username) alert('Please enter the Username');
     else if (!state.password) alert('Please enter the Password');
     else {
-      //url where post request will be made to
-      // Good place for login tests -> Check if username & password are truthy given an input
+      // Endpoint for login post request
       const url = 'api/user/login';
-      //in body we are sending object containing state which will get stringified and sent as JSON
       const option = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(state), // stringified to JSON {username: , password: }
       };
-      // fetching url to make POST request
+      // Post request
       fetch(url, option)
         .then((response) => response.json())
         .then((data) => {
+          // On successful login, redirect to MainPage.jsx & inject state into the component
           if (data.success) {
-            //if data is correct username/password -> pass fetched data as state
-            navigate('/home', { state: data.data }); // on successful login, re-route user to home route (MainPage.jsx) <- inject the state into the component
-          } else {
+            navigate('/home', { state: data.data });
+          } 
+          // On unsuccessful login, alert user and reset password field
+          else {
             alert(data.message);
-            setState({ ...state, password: '' }); // on unsuccessful login attempt, alert user and reset password field
+            setState({ ...state, password: '' });
           }
         });
     }
   };
-  // This is the changeHandler used in textfield component to change / update state
+
+  /**
+   * changeHandler - Updates state with the username and password as those fields are filled out
+   * @param {object} event 
+   */
   const changeHandler = (event) => {
-    // This setState is taking the previous state and only updating the name (username/password) with the value entered into input field
     setState({ ...state, [event.target.name]: event.target.value });
   };
 
